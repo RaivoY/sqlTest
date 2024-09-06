6c2cd77c6bf563dd5dabba6f52b9d1bc912ae9dc
Raivo <your@gmail.exe>
2024-09-06 10:56:42 +0300

    add all select files

---
+++ select/file2.sql
@@ -0,0 +1,11 @@
+use sql_store; 
+-- select
+-- 	first_name,
+--    last_name, 
+--    points, 
+--    points+10 as "points incresed"
+-- customers;
+
+select * from customers;
+SELECT DISTINCT state  FROM Customers;
+
---
+++ select/file3.sql
@@ -0,0 +1,9 @@
+use sql_store;
+-- select * from orders
+-- where order_date >= '2018-01-01' and order_date < '2019-01-01';
+
+select * from customers
+where state != 'VA';
+
+
+
